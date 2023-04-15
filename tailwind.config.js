2cd0eedb419e5ce02260a6a8ee1f8c14e997c097
Ross Gray <rossagray@gmail.com>
2023-04-15 15:09:17 +0300

    Update Tailwind Typography theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,11 @@
+const round = (num) =>
+  num
+    .toFixed(7)
+    .replace(/(\.[0-9]+?)0+$/, "$1")
+    .replace(/\.0$/, "");
+const rem = (px) => `${round(px / 16)}rem`;
+const em = (px, base) => `${round(px / base)}em`;
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{njk,md,html,js}"],
@@ -7,7 +15,42 @@ module.exports = {
       center: true,
       padding: "1rem",
     },
-    extend: {},
+    extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            a: {
+              color: theme("colors.blue.600"),
+              textDecoration: "none",
+              fontWeight: "inherit",
+              textDecorationThickness: "3px",
+            },
+            "a::hover": {
+              textDecoration: "underline",
+              textUnderlineOffset: "5px",
+            },
+            "code::before": {
+              content: "none",
+            },
+            "code::after": {
+              content: "none",
+            },
+            code: {
+              color: "#e2777a",
+            },
+            "* code": {
+              color: "#e2777a",
+            },
+            "blockquote p:first-of-type::before": {
+              content: "none",
+            },
+            "blockquote p:last-of-type::after": {
+              content: "none",
+            },
+          },
+        },
+      }),
+    },
   },
   // use typography plugin for markdown content
   plugins: [require("@tailwindcss/typography")],
